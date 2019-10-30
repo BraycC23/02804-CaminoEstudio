851ba22fb48191f1435cc31753bdbf1bfc5da44a
BRAYAN <BRAYAN@DESKTOP-NN30L1I.home>
2019-10-30 18:14:33 +0100

    Ejercicio 04 Tema 028

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,47 @@
+/* 
+Copyright 2019  Brayan C. Sanchez Arenas - brayancamilo.sanchez.alum2iescap.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brayan C. Sanchez Arenas
+ */
+public class Main {
+public static final Scanner SCN = 
+	  new Scanner(System.in, "Windows-1252")
+	  .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+    public static void main(String[] args) {
+        //Constantes 
+        final double distancia = 980 ;
+        //Variables
+        double dias;
+        double desplazamiento;
+        //Datos
+        try{
+            //Introducción de datos
+            System.out.print("Dezplazamiento diario (km) ...: ");
+            desplazamiento = SCN.nextDouble();
+            SCN.nextLine();
+            //Generar datos
+            dias = distancia / desplazamiento;
+            System.out.printf("Dias de Clases/Camino .......: %.1f%n", dias);
+    } catch (Exception e){
+            System.out.println("¡Datos Incorrectos!");
+    }
+}}
